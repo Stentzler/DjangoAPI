c1f5d0a7546257859ba3d4df05def55307e128f8
Victor Cazuriaga <vikktorrv@gmail.com>
2022-10-31 16:54:36 -0300

    feature: model exams

--- exams/models.py
+++ exams/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+import uuid
 # Create your models here.
+
+
+class Exams(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, editable=False,
+                          null=False, primary_key=True, unique=True)
+    name = models.CharField()
+    subjects = models.ForeignKey(
+        "subjects.Subject", related_name="exams", on_delete=models.CASCADE)
+    students = models.ForeignKey(
+        "students.Student", related_name="exams", on_delete=models.CASCADE)
+    report_cards = models.ForeignKey(
+        "report_cards.ReportCard", related_name="exams", on_delete=models.CASCADE)
