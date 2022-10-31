38d9c0f2d2f162063c3f886fd0aa739d2daac816
Jamily Lima <jamilylima70@gmail.com>
2022-10-31 18:14:35 -0300

    feat:create model grade

--- grades/models.py
+++ grades/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Grade(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    period = models.CharField(max_length=30, null=False, blank=False)
+    grade = models.CharField(max_length=20, null=False, blank=False)
+    class_name = models.CharField(max_length=30, null=False, blank=False)
+
+    subjects = models.ManyToManyField(
+        "subjects.Subject",
+        related_name="grades",
+    )
