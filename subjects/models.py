43a517e5ff8a0a7a959eab6a56cb96004f8f5259
Jamily Lima <jamilylima70@gmail.com>
2022-10-31 18:32:01 -0300

    feat:create model subject

--- subjects/models.py
+++ subjects/models.py
@@ -2,4 +2,9 @@ from django.db import models
 import uuid
 
 class Subject(models.Model):
-    ...
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    name = models.CharField(max_length=50, null=False, blank=False)
+
+    teacher = models.ForeignKey(
+        "teachers.Teacher", on_delete=models.CASCADE, related_name="Subjects"
+    )
