0b2666be3a89205868baa6076684f1605d5dbf31
Vinicius Stentzler <vin.stentzler@gmail.com>
2022-10-31 23:34:19 -0300

    feat: created admin_user/student_user/teacher_user // setting running on sqlite3

--- exams/models.py
+++ exams/models.py
@@ -1,14 +1,16 @@
 from django.db import models
 import uuid
-# Create your models here.
 
 
 class Exams(models.Model):
     id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
-    name = models.CharField()
+    name = models.CharField(max_length=66)
     subject = models.ForeignKey(
-        "subjects.Subject", related_name="exams", on_delete=models.CASCADE)
+        "subjects.Subject", related_name="exams", on_delete=models.CASCADE
+    )
     student = models.ForeignKey(
-        "students.Student", related_name="exams", on_delete=models.CASCADE)
+        "students.Student", related_name="exams", on_delete=models.CASCADE
+    )
     report_card = models.ForeignKey(
-        "report_cards.ReportCard", related_name="exams", on_delete=models.CASCADE)
+        "report_cards.ReportCard", related_name="exams", on_delete=models.CASCADE
+    )
