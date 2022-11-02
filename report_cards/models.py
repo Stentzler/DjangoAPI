1c1cae6522938348f28baa71d064578b5ea2649e
noomloof <bhtanikawa@gmail.com>
2022-11-01 21:23:22 -0400

    feat: adjusted report card

--- report_cards/models.py
+++ report_cards/models.py
@@ -5,16 +5,17 @@ import uuid
 
 class ReportCard(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
-    N1 = models.PositiveIntegerField(default=0, validators=[ MinValueValidator(0), MaxValueValidator(100)])
-    N2 = models.PositiveIntegerField(default=0, validators=[ MinValueValidator(0), MaxValueValidator(100)])
-    N3 = models.PositiveIntegerField(default=0, validators=[ MinValueValidator(0), MaxValueValidator(100)])
-    N4 = models.PositiveIntegerField(default=0, validators=[ MinValueValidator(0), MaxValueValidator(100)])
+    result_q1 = models.PositiveIntegerField(default=0, validators=[ MinValueValidator(0), MaxValueValidator(100)])
+    result_q2 = models.PositiveIntegerField(default=0, validators=[ MinValueValidator(0), MaxValueValidator(100)])
+    result_q3 = models.PositiveIntegerField(default=0, validators=[ MinValueValidator(0), MaxValueValidator(100)])
+    result_q4 = models.PositiveIntegerField(default=0, validators=[ MinValueValidator(0), MaxValueValidator(100)])
     is_approved = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
     average = models.DecimalField(default=0, max_digits=5, decimal_places=2)
     attendance = models.DecimalField(default=100.00, max_digits=5, decimal_places=2)
 
-    student = models.ForeignKey("custom_users.Student", related_name="report_cards" ,on_delete=models.CASCADE)
+    student = models.ForeignKey("custom_users.Student", on_delete=models.CASCADE)
+    subject = models.ForeignKey("subjects.Subject", on_delete=models.CASCADE)
 
 
     
