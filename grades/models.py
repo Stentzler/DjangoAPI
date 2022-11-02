5c43bd20c47d288912b2fe4e578569a45f9190dc
Vinicius Stentzler <vin.stentzler@gmail.com>
2022-11-01 21:27:26 -0300

    fix: relation with grades added to student

--- grades/models.py
+++ grades/models.py
@@ -9,13 +9,11 @@ class Grade(models.Model):
         NOITE = "NOITE", "Noturno"
 
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
-    period = models.CharField(max_length=30, null=False, blank=False)
-    class_name = models.CharField(max_length=30, null=False, blank=False)
-    grade = models.CharField(
-        max_length=20,
-        choices=Period.choices,
-        default=Period.MANHA,
+    period = models.CharField(
+        max_length=20, choices=Period.choices, default=Period.MANHA
     )
+    class_name = models.CharField(max_length=30, null=False, blank=False)
+    grade = models.CharField(max_length=30, null=False, blank=False)
 
     subjects = models.ManyToManyField(
         "subjects.Subject",
