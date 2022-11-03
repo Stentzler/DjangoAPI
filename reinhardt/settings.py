908d9631667c7d743bac7e963f99b0f0866c4287
noomloof <bhtanikawa@gmail.com>
2022-11-03 14:05:23 -0400

    heroku deploy v2.4
    Signed-off-by: noomloof <bhtanikawa@gmail.com>

--- reinhardt/settings.py
+++ reinhardt/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['reinhardt-mgmt.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['reinhardt-mgmt.herokuapp.com', 'localhost']
 # ^^^^^^ NEEDS TO BE UPDATED
 
 
