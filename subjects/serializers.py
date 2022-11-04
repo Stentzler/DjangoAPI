678cb4c832b66523f4000a06014c38a38c049542
Vinicius Stentzler <vin.stentzler@gmail.com>
2022-11-04 14:01:10 -0300

    feat: report_card being created for each subject the student is enroled

--- subjects/serializers.py
+++ subjects/serializers.py
@@ -7,12 +7,11 @@ from custom_users.serializers import TeacherName
 
 class SubjectsSerializer(serializers.ModelSerializer):
     id = serializers.CharField(read_only=True)
-    teacher = TeacherName()
 
     class Meta:
         model = Subject
         fields = [
-            'id',
-            'name',
-            'teacher',
+            "id",
+            "name",
+            "teacher",
         ]
