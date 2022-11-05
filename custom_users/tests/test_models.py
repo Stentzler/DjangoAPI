556a3533db5bd119d28716c8e386d99e337578f9
noomloof <bhtanikawa@gmail.com>
2022-11-04 21:20:43 -0400

    feat: added new tests and refactored into model bakery

--- custom_users/tests/test_models.py
+++ custom_users/tests/test_models.py
@@ -1,68 +1,73 @@
 from django.test import TestCase
 from custom_users.models import Student, Teacher
-from addresses.models import Address
-from grades.models import Grade
+from model_bakery import baker
 import datetime, uuid
-
+import ipdb
+from custom_users.tests.baker_recipes import student_custom, teacher_custom
+from django.db import IntegrityError
 
 class UsersModelTest(TestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.address_data = {
-            "zipcode": "123123",
-            "district": "Centro",
-            "state": "RS",
-            "street": "Rua",
-            "number": "123A",
-        }
+        ...
+        # cls.address_data = {
+        #     "zipcode": "123123",
+        #     "district": "Centro",
+        #     "state": "RS",
+        #     "street": "Rua",
+        #     "number": "123A",
+        # }
 
-        cls.grade_data = {
-            "class_name": "5a_A",
-            "grade": "5a Série",
-        }
+        # cls.grade_data = {
+        #     "class_name": "5a_A",
+        #     "grade": "5a Série",
+        # }
 
-        cls.student_data = {
-            "username": "student",
-            "rg": "999-999-999",
-            "first_name": "Stu",
-            "last_name": "Dent",
-            "age": 22,
-            "contacts": "Dona Angela",
-            "email": "student@mail.com",
-            "password": "1234",
-        }
+        # cls.student_data = {
+        #     "username": "student",
+        #     "rg": "999-999-999",
+        #     "first_name": "Stu",
+        #     "last_name": "Dent",
+        #     "age": 22,
+        #     "contacts": "Dona Angela",
+        #     "email": "student@mail.com",
+        #     "password": "1234",
+        # }
 
-        cls.teacher_data = {
-            "username": "teacher",
-            "rg": "999-999-999",
-            "first_name": "Tea",
-            "last_name": "Cher",
-            "age": 18,
-            "contacts": "Dona Angela",
-            "email": "teacher@mail.com",
-            "password": "1234",
-            "cpf": "12345678910",
-        }
-
-        cls.address = Address.objects.create(**cls.address_data)
-        cls.grade = Grade.objects.create(**cls.grade_data)
+        # cls.teacher_data = {
+        #     "username": "teacher",
+        #     "rg": "999-999-999",
+        #     "first_name": "Tea",
+        #     "last_name": "Cher",
+        #     "age": 18,
+        #     "contacts": "Dona Angela",
+        #     "email": "teacher@mail.com",
+        #     "password": "1234",
+        #     "cpf": "12345678910",
+        # }
+    def setUp(self):
+        self.address = baker.make("addresses.Address")
+        self.grade = baker.make("grades.Grade")
+        self.base_user = baker.make("custom_users.Student")
 
     def test_should_be_able_to_register_a_student(self):
         """trying to create an student providing correct data"""
-        student = Student.objects.create_user(
-            **self.student_data, address=self.address, grade=self.grade
-        )
+        # student = Student.objects.create_user(
+        #     **self.student_data, address=self.address, grade=self.grade
+        # )
+        student = baker.make_recipe("custom_users.tests.student_custom", address=self.address, grade=self.grade)
+        student_base_data = student_custom.__dict__
 
         self.assertIsInstance(student, Student)
         self.assertIsInstance(student.id, uuid.UUID)
         self.assertIsInstance(student.date_joined, datetime.datetime)
-        self.assertEqual(student.username, self.student_data["username"])
-        self.assertEqual(student.first_name, self.student_data["first_name"])
-        self.assertEqual(student.last_name, self.student_data["last_name"])
-        self.assertEqual(student.rg, self.student_data["rg"])
-        self.assertEqual(student.age, self.student_data["age"])
-        self.assertEqual(student.contacts, self.student_data["contacts"])
-        self.assertEqual(student.email, self.student_data["email"])
+        self.assertEqual(student.username, student_base_data["attr_mapping"]["username"])
+        self.assertEqual(student.first_name, student_base_data["attr_mapping"]["first_name"])
+        self.assertEqual(student.last_name, student_base_data["attr_mapping"]["last_name"])
+        self.assertEqual(student.rg, student_base_data["attr_mapping"]["rg"])
+        self.assertEqual(student.age, student_base_data["attr_mapping"]["age"])
+        self.assertEqual(student.contacts, student_base_data["attr_mapping"]["contacts"])
+        self.assertEqual(student.email, student_base_data["attr_mapping"]["email"])
         self.assertEqual(student.role, "STUDENT")
         self.assertEqual(student.address.id, self.address.id)
         self.assertEqual(student.grade.id, self.grade.id)
@@ -73,21 +78,66 @@ class UsersModelTest(TestCase):
     def test_should_be_able_to_register_a_teacher(self):
         """trying to create an student passing correct data"""
 
-        teacher = Teacher.objects.create_user(**self.teacher_data, address=self.address)
+        # teacher = Teacher.objects.create_user(**self.teacher_data, address=self.address)
+        teacher = baker.make_recipe("custom_users.tests.teacher_custom", address=self.address)
+        teacher_base_data = teacher_custom.__dict__
 
         self.assertIsInstance(teacher, Teacher)
         self.assertIsInstance(teacher.id, uuid.UUID)
         self.assertIsInstance(teacher.date_joined, datetime.datetime)
-        self.assertEqual(teacher.username, self.teacher_data["username"])
-        self.assertEqual(teacher.first_name, self.teacher_data["first_name"])
-        self.assertEqual(teacher.last_name, self.teacher_data["last_name"])
-        self.assertEqual(teacher.rg, self.teacher_data["rg"])
-        self.assertEqual(teacher.age, self.teacher_data["age"])
-        self.assertEqual(teacher.contacts, self.teacher_data["contacts"])
-        self.assertEqual(teacher.email, self.teacher_data["email"])
-        self.assertEqual(teacher.cpf, self.teacher_data["cpf"])
+        self.assertEqual(teacher.username, teacher_base_data["attr_mapping"]["username"])
+        self.assertEqual(teacher.first_name, teacher_base_data["attr_mapping"]["first_name"])
+        self.assertEqual(teacher.last_name, teacher_base_data["attr_mapping"]["last_name"])
+        self.assertEqual(teacher.rg, teacher_base_data["attr_mapping"]["rg"])
+        self.assertEqual(teacher.age, teacher_base_data["attr_mapping"]["age"])
+        self.assertEqual(teacher.contacts, teacher_base_data["attr_mapping"]["contacts"])
+        self.assertEqual(teacher.email, teacher_base_data["attr_mapping"]["email"])
+        self.assertEqual(teacher.cpf, teacher_base_data["attr_mapping"]["cpf"])
         self.assertEqual(teacher.role, "TEACHER")
         self.assertEqual(teacher.address.id, self.address.id)
         self.assertTrue(teacher.is_active)
         self.assertFalse(teacher.is_superuser)
         self.assertFalse(teacher.is_staff)
+    
+    def test_user_username_max_length(self):
+        max_length = self.base_user._meta.get_field('username').max_length
+
+        self.assertEqual(max_length, 150)
+
+    def test_user_first_name_max_length(self):
+        max_length = self.base_user._meta.get_field('first_name').max_length
+
+        self.assertEqual(max_length, 150)
+
+    def test_user_last_name_max_length(self):
+        max_length = self.base_user._meta.get_field('last_name').max_length
+
+        self.assertEqual(max_length, 150)
+
+    def test_user_role_max_length(self):
+        max_length = self.base_user._meta.get_field('role').max_length
+
+        self.assertEqual(max_length, 50)
+
+    def test_user_contacts_max_length(self):
+        max_length = self.base_user._meta.get_field('contacts').max_length
+
+        self.assertEqual(max_length, 70)
+
+    def test_user_password_max_length(self):
+        max_length = self.base_user._meta.get_field('password').max_length
+
+        self.assertEqual(max_length, 70)
+
+    def test_user_rg_max_length(self):
+        max_length = self.base_user._meta.get_field('rg').max_length
+
+        self.assertEqual(max_length, 20)
+
+    def test_user_age_does_not_accept_negative_values(self):
+        with self.assertRaises(IntegrityError):
+            baker.make("custom_users.Student", age=-34)
+
+
+
+
