f66e70176c8aa683abe811ddc25603f05306cb4e
God bless your rotten soul <caionovo10@gmail.com>
2022-11-07 02:31:28 -0300

    feat:update subject teacher route

--- subjects/serializers.py
+++ subjects/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from custom_users.serializers import TeacherSerializer
-
+from subjects.exeptions import NonUpdatableKeyError
 from subjects.models import Subject
 from custom_users.serializers import TeacherName
 
@@ -15,3 +15,24 @@ class SubjectsSerializer(serializers.ModelSerializer):
             "name",
             "teacher",
         ]
+        
+class SubjectsPatchTeacherSerializer(serializers.ModelSerializer):  
+    id = serializers.CharField(read_only=True)
+    class Meta:    
+        model = Subject
+        fields = [
+            "id",
+            "name",
+            "teacher",
+           ]
+    def update(self, instance, validated_data):
+        
+        for key, value in validated_data.items():
+            if key=="name":
+                raise NonUpdatableKeyError({"name": "You can not update name property in this route."})
+            
+            setattr(instance,key, value )
+       
+        instance.save()
+        return instance
+ 
