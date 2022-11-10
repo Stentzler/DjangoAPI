565ac8686afdce54763d93fa0f9bd8dd20f0e5d6
noomloof <bhtanikawa@gmail.com>
2022-11-10 13:06:29 -0400

    fix: adjusted 500 error, should be last push

--- exams/serializers.py
+++ exams/serializers.py
@@ -43,10 +43,10 @@ class ExamsSerializer(serializers.ModelSerializer):
                         date=data,
                     )
                 return exams_created
-            else:
-                raise Unauthorized(
-                    {"message": "This subject does not belong to this grades."}
-                )
+        
+        raise Unauthorized(
+            {"message": "This subject does not belong to this grades."}
+        )
                 
                 
 class ExamsGetSerializer(serializers.ModelSerializer):
